f52bcf0ede1bcf6d448ed170a616d34f3fb7d538
nhatvy1 <vyhuynhak123@gmail.com>
2024-02-24 23:14:47 +0700

    upload

--- src/hooks/useFirestore.tsx
+++ src/hooks/useFirestore.tsx
@@ -3,8 +3,10 @@ import { useEffect } from "react";
 import { db } from "../firebase/config";
 
 const useFirestore = async (_collection: string, condition: string | null) => {
-  const query =
-  let collectionRef = collection(db, _collection).orderBy('name', 'desc').get();
+  const collectionRef = collection(db, _collection)
+
+// Order by the "name" field in ascending order
+const query = collectionRef.orderBy("name");
   useEffect(() => {
     onSnapshot(collection(db, "rooms"), (snapshot: any) => {
       const data = snapshot.docs.map((doc: any) => ({
